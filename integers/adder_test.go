c8ae2b6ed00504a3999d798bbd045df73d25c3c2
Arturo Simpson <artsim3088@gmail.com>
2026-01-15 13:22:47 -0500

    Code for the integer section

---
+++ integers/adder_test.go
@@ -0,0 +1,22 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Doesn't seem to be working as intended. Leaving this commented out. May or may not come back to this. 
+	// Output: 6
+}
