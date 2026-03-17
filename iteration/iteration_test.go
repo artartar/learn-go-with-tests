782ac52db874bfeae1e6fc00a2196e247b75e14f
artartar <artsim3088@gmail.com>
2026-03-17 10:03:28 -0400

    Update iteration_test.go
    Just writing something, not tested, does not work

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -10,6 +10,11 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but %q", expected, repeated)
 	}
 }
+# Just getting myself to write some kind of code, even the function below
+func TestVarRepeat(t *testing.T, x int) {
+	x = 10
+	repeated := Repeat("a", x)
+	expected := "a" * x
 
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
